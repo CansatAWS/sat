11d1ebdd9d0af2bd30c328f458f5581c912808f4
Jameswflynn <60551810+Jameswflynn@users.noreply.github.com>
2020-03-17 00:48:28 +0000

    Update radiocomm.py
    Simplified format and packet is stored locally

--- radiocomm.py
+++ radiocomm.py
@@ -1,17 +1,14 @@
-import math
+def sendMessage(name, sensor, message, max_packet_size = 40):
+    packet = "<" + message +","+ sensor +"," + name +">"
+    packets = []
+    for i in range (0,len(packet), max_packet_size):
+        packets.append(packet[i,max_packet_size*i])
 
-def sendMessage(message, mType, name, max_packet_size = 40):
-    header = ",name:" + name+",type:" + mType + "," 
-    header = "hl:"+ str(int(((len(header)+ max_packet_size + math.log(len(header)-len(header)%10,10))/max_packet_size)))+",pl:" + str(int((max_packet_size+len(message))/max_packet_size)) + header
-    return header
-#hl,pl,packet,time,
-#after len(hlarray) == hl
-#assemble
-#messageparts
-#when len(plarray) == pl
-#decode
-#end
-print(sendMessage("Messageaaaaaaaaaaaaaaaaaaaaaa5y5jjnerfgg46", "string","0011155314754j67uk8ofgt54g56h55"))
-
-def assembleMessage(header,body):
+def constructReceived(packet):
+    packet = packet[1:len(packet)-1] +("\n")
+    packets = packet.split(",")
+    file = open(packets[1]+".txt","a+")
+    file.write(packet)
     
+    
+print(constructReceived("<name,data, sensor,>"))
